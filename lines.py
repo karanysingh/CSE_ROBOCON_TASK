3df1cb48f7d67f3e6a0f2ff76e64d7c369c3de8d
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-04-20 16:34:14 +0530

    Update lines.py

--- lines.py
+++ lines.py
@@ -15,9 +15,14 @@ while True:
                 x1, y1, x2, y2 = line[0]
                 cv2.line(final_frame, (x1-5, y1), (x2+5, y2), (0, 255, 0), 10)
                 edges2 = cv2.Canny(final_frame, 100, 400)
-                cv2.rectangle(frame,(x1-50,y1-20), (x2+50,y2+20), (0,255,0), 1)
+                lines = cv2.HoughLinesP(edges2, 1, np.pi / 180, 200)
+                if lines is not None:
+                    for line in lines:
+                        x1, y1, x2, y2 = line[0]
+                        cv2.rectangle(frame,(x1-50,y1-20), (x2+50,y2+20), (0,255,0), 1)
         cv2.imshow("frame", frame)
         cv2.imshow("edges", edges)
         cv2.imshow("edges2", edges2)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+#To use IP webcam just replace ‘0’ in line4 by url of camera feed.
