dd4f3274bb630d22a646ae9dd06cd0cdde6bd475
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-04-20 18:20:49 +0530

    Add files via upload

---
+++ client-server/server.py
@@ -0,0 +1,17 @@
+#server file
+
+import socket              
+
+s = socket.socket()         
+host = socket.gethostname() 
+port = 12345                
+s.bind((host, port))        
+
+s.listen(5)
+while True:
+    c, addr = s.accept() 
+    print('Got connection from\n')
+    data = input('Enter the data you want to write to file:\n')
+    c.send(data.encode())
+    print(c.recv(1024))
+c.close()
