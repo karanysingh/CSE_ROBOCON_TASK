ab7eb7538e324b69fa33dfa497a60cc8d1075d6d
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-04-30 01:20:28 +0530

    Add files via upload

---
+++ Arduino/MPU6050_RAW_VALUES.ino
@@ -0,0 +1,33 @@
+#include<Wire.h>
+const int MPU = 0x68;
+int16_t AccX,AccY,AccZ,Temp,GyroX,GyroY,GyroZ;
+void setup() {
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);//Device reset
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);//ACCEL_XOUT_H  -->register name
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,14,true);
+  AccX=Wire.read()<<8|Wire.read();
+  AccY=Wire.read()<<8|Wire.read();
+  AccZ=Wire.read()<<8|Wire.read();
+  Temp=Wire.read()<<8|Wire.read();
+  GyroX=Wire.read()<<8|Wire.read();
+  GyroY=Wire.read()<<8|Wire.read();
+  GyroZ=Wire.read()<<8|Wire.read();
+  Serial.print("AccX = "); Serial.print(AccX);
+  Serial.print("  AccY = "); Serial.print(AccY);
+  Serial.print("  AccZ = "); Serial.print(AccZ);
+  Serial.print("\nTemp = "); Serial.print(Temp/340.00+36.53);
+  Serial.print("\nGyroX = "); Serial.print(GyroX);
+  Serial.print("  GyroY = "); Serial.print(GyroY);
+  Serial.print("  GyroZ = "); Serial.println(GyroZ);
+  delay(200);
+}
---
+++ Opencv-line detection/lines.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+# READING FRAMES
+video = cv2.VideoCapture(0, cv2.CAP_V4L)
+while True:
+    success, frame = video.read()
+    if success==True:
+        final_frame = frame.copy()
+        gray = cv2.cvtColor(final_frame, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, 100, 400)
+        edges2 = edges.copy()#doubling the layer
+        lines = cv2.HoughLinesP(edges2, 1, np.pi / 180, 200)
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(final_frame, (x1-5, y1), (x2+5, y2), (0, 255, 0), 10)
+                edges2 = cv2.Canny(final_frame, 100, 400)
+                lines = cv2.HoughLinesP(edges2, 1, np.pi / 180, 200)
+                if lines is not None:
+                    for line in lines:
+                        x1, y1, x2, y2 = line[0]
+                        cv2.rectangle(frame,(x1-50,y1-20), (x2+50,y2+20), (0,255,0), 1)
+        cv2.imshow("frame", frame)
+        cv2.imshow("edges", edges)
+        cv2.imshow("edges2", edges2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+#To use IP webcam just replace ‘0’ in line4 by url of camera feed.
