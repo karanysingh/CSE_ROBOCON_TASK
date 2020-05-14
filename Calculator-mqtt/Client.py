afebaff844491925a33da6d2e4a67c3de70439c0
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-05-14 21:55:58 +0530

    Add files via upload

--- Calculator-mqtt/Client.py
+++ Calculator-mqtt/Client.py
@@ -1,8 +1,5 @@
 import paho.mqtt.client as mqtt
 import time
-def on_log (client, userdata, level, msg):
-    # print("Logged!!"+str(msg))
-    pass
 
 def on_connect (client, userdata, flags, rc):
     if rc == 0:
@@ -11,15 +8,15 @@ def on_connect (client, userdata, flags, rc):
         print("Bad Connection!")
 
 def on_message (client, userdata, msg):
-    print("message :  " + msg.payload.decode())
+    print(msg.payload.decode())
+    opt = input()
+    client.publish("calc/opts",opt)
 
 
 client = mqtt.Client("calculator")
 client.on_connect = on_connect
-client.on_log = on_log
 client.on_message = on_message
 print("Establishing Connection!!")
 client.connect('192.168.43.85', 8080, 60)
-# client.loop_start()
 client.subscribe("calc/welcome")
 client.loop_forever()
--- Calculator-mqtt/Publisher.py
+++ Calculator-mqtt/Publisher.py
@@ -1,7 +1,27 @@
 import paho.mqtt.client as mqtt
 import time
+def on_message (client, userdata, msg):
+    opt = msg.payload.decode()
+    print("Got --> "+opt)
+    try:
+        opt = int(opt)
+    except:
+        pass
+    if(opt==1):
+        print("Addition")
+    elif(opt==2):
+        print("Substraction")
+    elif(opt==3):
+        print("Multiplication")
+    elif(opt==4):
+        print("Division")
+    else:
+        print("Invalid Input!!")
+
 client = mqtt.Client("publisher")
+client.on_message = on_message
 client.connect('192.168.43.85', 8080, 60)
-client.publish("calc/welcome", "Hi!!this is a mqtt based calculator")
-# client.loop_forever()
-client.disconnect()
+client.subscribe("calc/opts")
+client.publish("calc/welcome", "Hi!! This is a mqtt-based calculator \nWhat operation do you want to perform? \n 1)Addition 2)Substraction\n 3)Multiplication 4)Division")
+client.loop_forever()
+# client.disconnect()
